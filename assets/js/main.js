b449cb72a28f56a7953cdb58b62f4975f64e61c0
aggyjlee <agneslee2022@u.northwestern.edu>
2024-04-19 23:21:04 -0400

    feat: javascript for image gallery

--- assets/js/main.js
+++ assets/js/main.js
@@ -98,13 +98,13 @@
 		// Lightbox gallery.
 			$window.on('load', function() {
 
-				$('#two').poptrox({
-					caption: function($a) { return $a.next('h3').text(); },
+				$('#project-gallery').poptrox({
+					// caption: function($a) { return $a.next('h3').text(); },
 					overlayColor: '#2c2c2c',
 					overlayOpacity: 0.85,
 					popupCloserText: '',
 					popupLoaderText: '',
-					selector: '.work-item a.image',
+					selector: 'a.gallery-test',
 					usePopupCaption: true,
 					usePopupDefaultStyling: false,
 					usePopupEasyClose: false,
